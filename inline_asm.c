24f7649f0ed238c2b8f9fde76e2c92a94e2bb6bd
jxwleong <jason.xie.wei.leong@gmail.com>
2021-10-19 19:27:21 +0800

    Change movl => movq, addl => add and use "r64" register instead of "e32" to solve the error C:\Users\jason\AppData\Local\Temp\cceoAOpV.s: Assembler messages: C:\Users\jason\AppData\Local\Temp\cceoAOpV.s:58: Error: unsupported instruction `mov'
    Reference: https://stackoverflow.com/a/68844042

--- inline_asm.c
+++ inline_asm.c
@@ -4,12 +4,12 @@
 int main(){
     long long value;
     /*
-        1. Move 0x10 to ebx
-        2. Add 0x10 with value to ebx (0x10)
+        1. Move 0x10 to rbx
+        2. Add 0x10 with value to rbx (0x10)
     */
-    __asm__("movl $0x10, %ebx;"
-            "addl $0x10, %ebx;");
-    __asm__("movl %%ebx, %0;" : "=r" (value) : );
+    __asm__("movq $0x10, %rbx;"
+            "add $0x10, %rbx;");
+    __asm__("movq %%rbx, %0;" : "=r" (value) : );
     printf("0x%x", value);
     return 0;
 }
